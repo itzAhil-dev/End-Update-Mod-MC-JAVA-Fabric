0c26a7562f356aaaa4024f51274ce1b7431a64d0
Lenovo <itzahil11@outlook.com>
2025-10-11 19:05:46 +0530

    initial commit

---
+++ src/main/java/net/itzshaiky/EndUpdate.java
@@ -0,0 +1,20 @@
+package net.itzshaiky;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndUpdate implements ModInitializer {
+	public static final String MOD_ID = "end-update";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
